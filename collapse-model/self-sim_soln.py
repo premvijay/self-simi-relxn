e35ca07ef21b9427f1277e1bfbdaaa715a912137
PremVijay Velmani <premvpv18@gmail.com>
2022-09-09 22:41:24 +0530

    trajectory evolution time adapt for s and misc

--- collapse-model/self-sim_soln.py
+++ collapse-model/self-sim_soln.py
@@ -13,18 +13,18 @@ from time import time
 
 # %%
 s = 1
-fig4, ax4 = plt.subplots(1, dpi=200, figsize=(10,7))
+fig4, (ax4,ax41) = plt.subplots(2, dpi=200, figsize=(10,12), sharex=True)
 fig5, (ax5,ax6) = plt.subplots(1,2, dpi=200, figsize=(14,7))
 
 t_now = time()
-for s in [0.5,1,1.5,2,3][1::]:
+for s in [0.5,1,1.5,2,3][::]:
     de = 2* (1+s/3) /3
+    upsil = 1 if s >= 3/2 else 3*s/(s+3)
 
     def M0(l):
         return l**(3/4)
     def M_pred(l):
-        gam = 1 if s >= 3/2 else 3*s/(s+3)
-        return l**gam
+        return l**upsil
 
     # def M(l,n):
     #     return 0
@@ -33,13 +33,13 @@ for s in [0.5,1,1.5,2,3][1::]:
 
     color_this = plt.cm.jet(s/3)
     # linestyles = [":","-.","--","-"]
-    linestyles = [(0, (1, 3)), (0, (2, 3)), (0, (3, 3)), (0, (4, 3)), (0, (5, 1,1,1)), 'solid']
+    linestyles = [(0, (1, 3)), (0, (2, 3)), (0, (3, 3)), (0, (4, 3)), (0, (5, 1,1,1)), 'solid'][1:]
     ls_cycler = cycle(linestyles[::])
 
     t_bef, t_now = t_now, time()
     print(f'{t_now-t_bef:.4g}s', f's={s} Initialised vals and funcs for iteration')
 
-    for n in range(9):
+    for n in range(5):
         def ode_func(xi, arg):
             lam = arg[0]
             v = arg[1]
@@ -51,8 +51,9 @@ for s in [0.5,1,1.5,2,3][1::]:
             #     print(lam,s, v, xi)
             #     raise Exception
 
+        xi_max = np.log(5e-4**upsil)*-3/2/s
 
-        res = solve_ivp(ode_func, (0,4), np.array([1,-de]), method='Radau', t_eval=np.linspace(0,64,5000000)**(1/3), max_step=np.inf, dense_output=False, vectorized=True) #np.unique(np.concatenate([np.linspace(0,2,50000),np.log10(np.linspace(1,10000,100000))]))
+        res = solve_ivp(ode_func, (0,xi_max), np.array([1,-de]), method='Radau', t_eval=np.linspace(0,xi_max**3,500000)**(1/3), max_step=np.inf, dense_output=False, vectorized=True) #np.unique(np.concatenate([np.linspace(0,2,50000),np.log10(np.linspace(1,10000,100000))]))
         # res1 = solve_ivp(fun, (res.t[-1],15), np.array([res.y[0][-1],-res.y[1][-1]]), max_step=0.1, dense_output=True)
 
         xi = res.t
@@ -84,11 +85,12 @@ for s in [0.5,1,1.5,2,3][1::]:
         l_range[1:] = np.logspace(-2.5,0, 300)
         n_roots_ar = np.zeros(301)
         # first_root = [0]
-        all_roots = [[None]] #np.zeros(300,70)
+        # all_roots = [[None]] #np.zeros(300,70)
         M_vals = np.zeros(301)
         # M_vals_er = []
-        rho_vals = np.zeros(301)
-        v_xi = interp1d(xi, v, fill_value="extrapolate")
+        if n==4:
+            rho_vals = np.zeros(301)
+            v_xi = interp1d(xi, v, fill_value="extrapolate")
         for i in range(1,301):
             # l_range.append(l)
             l = l_range[i]
@@ -104,18 +106,19 @@ for s in [0.5,1,1.5,2,3][1::]:
             M_vals[i] = M_val
             n_roots_ar[i] = n_roots
             # first_root.append(roots[0])
-            all_roots.append(roots)
+            # all_roots.append(roots)
             # if roots.shape[0]>1:
             #     M_vals_er.append(Int_M[-2]-Int_M[-1])
             # else:
             #     M_vals_er.append(0)
-            Int_rho = 2/9*s * (3*np.pi/4)**2 * np.exp((-2*s/3)*roots) / np.abs(v_xi(roots)) / l**2
-            rho_vals[i] = np.sum(Int_rho)
+            if n==4:
+                Int_rho = 2/9*s * (3*np.pi/4)**2 * np.exp((-2*s/3)*roots) / np.abs(v_xi(roots)) / l**2
+                rho_vals[i] = np.sum(Int_rho)
         M_vals[-1] = 1
 
         M_vals = np.asarray(M_vals)
         # M_vals_er = np.asarray(M_vals_er)
-        rho_vals = np.asarray(rho_vals)
+        if n==4: rho_vals = np.asarray(rho_vals)
 
         M_vals /= M_vals[-1]
 
@@ -125,12 +128,12 @@ for s in [0.5,1,1.5,2,3][1::]:
         print(f'{t_now-t_bef:.4g}s', f'{n}th iter DM mass profile obtained')
 
 
-        df = pd.DataFrame(data={'l':l_range, 'M':M_vals, 'rho':rho_vals,})
+        df = pd.DataFrame(data={'l':l_range, 'M':M_vals,})
         df.to_hdf(f'profiles_dmo_{s}.hdf5', 'main')
         df.to_hdf(f'profiles_dmo_{s}.hdf5', f'iter{n}')
         
 
-        if n in [0,2,4,5,7,8]:
+        if n in [0,1,2,3,4,5,7,8]:
             # xi = np.linspace(0,4,100)
             # lam = res.sol(xi)[0]
             # plt.plot(xi,lam)
@@ -143,6 +146,7 @@ for s in [0.5,1,1.5,2,3][1::]:
             if n==6: ls='-'
             
             ax4.plot(xi,lam, color=color_this, ls=ls, lw=1)
+            ax41.plot(xi,v, color=color_this, ls=ls, lw=1)
             # plt.plot(xi,lamF, color=color_this, label=f's={s}')
             # plt.plot(res1.t, res1.y[0], color=color_this)
             # plt.plot(res.t, res.y[1], color=color_this)
@@ -151,10 +155,11 @@ for s in [0.5,1,1.5,2,3][1::]:
             if s==1:
                 ax5.plot([],[], color='k', ls=ls, label=f'n={n}', lw=1)
                 ax4.plot([],[], color='k', ls=ls, label=f'n={n}', lw=1)
+                ax41.plot([],[], color='k', ls=ls, label=f'n={n}', lw=1)
             
             ax5.plot(l_range, M_vals, color=color_this, ls=ls, lw=1)
             # ax6.plot(l_range[1:], np.diff(M_vals)/l_range[1:]**2, color=color_this, ls=ls, lw=1)
-            if n==2: ax6.plot(l_range[1:], rho_vals[1:], color=color_this, ls='-', lw=1)
+            if n==4: ax6.plot(l_range[1:], rho_vals[1:], color=color_this, ls='-', lw=1)
 
             t_bef, t_now = t_now, time()
             print(f'{t_now-t_bef:.4g}s', f'{n}th iter saved and plotted')
@@ -165,7 +170,7 @@ for s in [0.5,1,1.5,2,3][1::]:
 
     eps = 1/s
     Mta = (3*np.pi/4)**2
-    tht = np.linspace(0.01, 2*np.pi,300)
+    tht = np.linspace(np.pi, 2*np.pi,300)
     Mn = ((tht-np.sin(tht))/np.pi)**(-2/3*s)
     M = Mta * Mn
     lam_param = (1-np.cos(tht))/2 * Mn**(1/3+eps)
@@ -176,21 +181,24 @@ for s in [0.5,1,1.5,2,3][1::]:
     
 
 
-all_roots_ar = np.zeros((int(max(n_roots_ar)),301))
-for i,roots_ar in enumerate(all_roots):
-    all_roots_ar[0:len(roots_ar),i] = roots_ar
+# all_roots_ar = np.zeros((int(max(n_roots_ar)),301))
+# for i,roots_ar in enumerate(all_roots):
+#     all_roots_ar[0:len(roots_ar),i] = roots_ar
 
 
-ax4.set_xlim(0,4)
-ax4.set_ylim(0,1)
+ax4.set_xlim(0,10)
+ax4.set_ylim(5e-4,1)
 ax4.set_yscale('log')
-ax4.set_ylim(l_range[1],1)
+# ax4.set_ylim(l_range[1],1)
 ax4.xaxis.get_ticklocs(minor=True)     # []
 ax4.minorticks_on()
 ax4.grid(visible=True, which='both', axis='x')
 ax4.set_xlabel(r'$\xi$')
 ax4.set_ylabel(r'$\lambda$')
 ax4.legend()
+
+ax41.set_xlabel(r'$\xi$')
+ax41.set_ylabel(r'$v$')
 
 ax5.set_xlim(l_range[1]/1.2,1.1)
 ax5.set_ylim(M_vals[10]/2,1.1)
@@ -200,8 +208,8 @@ ax5.set_xlabel(r'$\lambda$')
 ax5.set_ylabel(r'$M$')
 ax5.legend()
 
-ax6.set_xlim(10**-1.5,1)
-ax6.set_ylim(0.7,10**5)
+ax6.set_xlim(l_range[1]/1.2,1.1)
+ax6.set_ylim(bottom=0.7)
 ax6.set_xscale('log')
 ax6.set_yscale('log')
 ax6.set_xlabel(r'$\lambda$')
